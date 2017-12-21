0efd110c13027f643ede2cb468297608f443ce19
Jzuo <804018306@qq.com>
2017-12-21 06:13:52 +0000

    Finish layout and routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
+//主页
+Route::get('/','StaticPagesController@home')->name('home');
+//帮助页
+Route::get('/help','StaticPagesController@help')->name('help');
+//关于页
+Route::get('/about','StaticPagesController@about')->name('about');
+//注册页面
+Route::get('/signup','UsersController@create')->name('signup');
