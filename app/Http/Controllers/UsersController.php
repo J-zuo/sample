330ffddb8df1fccd0216392e0ea96181e1ca3d1b
Jzuo <804018306@qq.com>
2017-12-22 09:08:31 +0000

    Finish user list

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,23 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
+
     public function create()
     {
         return view('users.create');
@@ -40,6 +57,7 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
@@ -49,6 +67,8 @@ class UsersController extends Controller
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
         ]);
+
+        $this->authorize('update', $user);
 
         $data = [];
         $data['name'] = $request->name;
