c8904f6866cec652688b17d39291f9edeec6b9fc
Jzuo <804018306@qq.com>
2018-03-18 12:25:20 +0000

    密码重设相关

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -33,7 +33,7 @@ class SessionsController extends Controller
             return redirect()->intended(route('users.show', [Auth::user()]));
         }else{
             Auth::logout();
-            session->flash('warning','您的账号未激活，请检查邮箱中的邀请码');
+            session()->flash('warning','您的账号未激活，请检查邮箱中的邀请码');
             return redirect('/');
         }
 
